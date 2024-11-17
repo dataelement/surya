4d4b5257b0d113c43d1b3897ed64ef130b679e10
youjiachen <youmcgrady@126.com>
2024-11-17 11:46:37 +0800

    update vis layout result

--- benchmark/layout_yolo.py
+++ benchmark/layout_yolo.py
@@ -4,71 +4,241 @@ import copy
 import json
 import os
 import time
+from enum import Enum
+from pathlib import Path
+from typing import Dict
 
-import datasets
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageFile
+
+from surya.postprocessing.visualization import visualize_bbox
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 from tabulate import tabulate
+from tqdm import tqdm
 
-from surya.benchmark.metrics import precision_recall
-from surya.detection import batch_text_detection
+from surya.benchmark.metrics import precision_recall_v2
 from surya.input.processing import convert_if_not_rgb
-from surya.layout import batch_layout_detection_yolo
-from surya.model.detection.model import load_model as load_det_model
-from surya.model.detection.model import load_processor as load_det_processor
+from surya.layout import batch_layout_detection, batch_layout_detection_yolo
+from surya.model.layout.model import load_model as load_surya_layout_model
+from surya.model.layout.model import load_processor as load_surya_layout_processor
 from surya.model.layout.yolo_model import load_model as load_layout_yolo_model
 from surya.postprocessing.heatmap import draw_bboxes_on_image
 from surya.settings import settings
 
 
+class ModelType(str, Enum):
+    DOCLAYOUT_YOLO = "doclayout_yolo"
+    ELEM_YOLO = "elem_yolo"
+    SURYA = "surya"
+
+
+class DatasetType(str, Enum):
+    ELEM = "elem"
+    DOCLAYNET = "doclaynet"
+
+
+def get_model_and_processor(model_type: ModelType):
+    """Get model and processor based on model type"""
+    if model_type == ModelType.DOCLAYOUT_YOLO:
+        return load_layout_yolo_model(), None
+    elif model_type == ModelType.ELEM_YOLO:
+        return load_layout_yolo_model(), None
+    elif model_type == ModelType.SURYA:
+        return load_surya_layout_model(), load_surya_layout_processor()
+
+
+def get_label_alignment(dataset_type: DatasetType, model_type: ModelType):
+    """Get label alignment based on dataset and model type"""
+    alignments = {
+        DatasetType.ELEM: {
+            ModelType.DOCLAYOUT_YOLO: {  # elem layout to doclayout_yolo
+                "Image": [["Seal", "Picture"], ["figure"]],
+                "Table": [["Table"], ["table"]],
+                "Text": [["Text"], ["plain text"]],
+                "Title": [["Title"], ["title"]],
+            },
+            ModelType.SURYA: {  # elem layout to surya
+                "Image": [["Picture"], ["Picture", "Figure"]],
+                "Table": [["Table"], ["Table"]],
+                "Text": [["Text"], ["Text"]],
+                "Title": [["Title"], ["Section-header", "Title"]],
+            },
+            ModelType.ELEM_YOLO: {  # elem layout to elem_yolo
+                "Image": [["Picture"], ["Picture", "Figure"]],
+                "Table": [["Table"], ["Table"]],
+                "Text": [["Text"], ["Text"]],
+                "Title": [["Title"], ["Section-header", "Title"]],
+            },
+        },
+        DatasetType.DOCLAYNET: {
+            ModelType.SURYA: {  # doclaynet to surya
+                "Image": [["Picture"], ["Picture"]],
+                "Table": [["Table"], ["Table"]],
+                "Text": [["Text"], ["Text"]],
+                "Title": [["Section-header", "Title"], ["Section-header", "Title"]],
+            }
+        },
+    }
+    return alignments[dataset_type][model_type]
+
+
+def get_dataset_id2label(dataset_type: DatasetType) -> Dict[int, str]:
+    """Get id2label mapping based on dataset type"""
+    mappings = {
+        DatasetType.ELEM: {
+            1: 'Seal',
+            2: 'Picture',
+            3: 'Title',
+            4: 'Text',
+            5: 'Table',
+            6: 'Page-header',
+            7: 'Page-number',
+            8: 'Page-footer',
+        },
+        DatasetType.DOCLAYNET: {
+            0: 'Caption',
+            1: 'Footnote',
+            2: 'Formula',
+            3: 'List-item',
+            4: 'Page-footer',
+            5: 'Page-header',
+            6: 'Picture',
+            7: 'Section-header',
+            8: 'Table',
+            9: 'Text',
+            10: 'Title',
+        },
+    }
+    return mappings[dataset_type]
+
+
+def load_layout_dataset(dataset_path: str, dataset_type: DatasetType, max_samples: int) -> pd.DataFrame:
+    """
+    Yolo format dataset is stored in a directory with the following structure:
+    dataset_path/
+        images/
+            *.jpg/png/etc
+        labels/
+            *.txt
+        train.txt
+        val.txt
+    """
+    # elem layout id2name
+    id2label = get_dataset_id2label(dataset_type)
+
+    with open(Path(dataset_path) / 'val.txt', 'r') as f:
+        lines = [line.strip() for line in f]
+
+    data = []
+    for idx, line in enumerate(tqdm(lines[:max_samples], desc="Loading dataset")):
+        row = dict(id=idx)
+        image_path = Path(dataset_path) / f'{line}'
+        label_path = Path(dataset_path) / 'labels' / f'{Path(line).stem}.txt'
+        row['image'] = Image.open(image_path)
+        width, height = row['image'].size
+
+        bboxes = []
+        category_ids = []
+        labels = []
+        with open(label_path, 'r') as f:
+            for line in f:
+                cls_id, *n_bbox = map(float, line.split())
+                xyxy = [n_bbox[0] * width, n_bbox[1] * height, n_bbox[4] * width, n_bbox[5] * height]
+                bboxes.append(xyxy)
+                category_ids.append(int(cls_id))
+                labels.append(id2label[int(cls_id)])
+
+        row['bboxes'] = bboxes
+        row['category_ids'] = category_ids
+        row['labels'] = labels
+        data.append(row)
+
+    return pd.DataFrame(data, index=range(len(data)))
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Benchmark surya layout model.")
+    parser = argparse.ArgumentParser(description="Benchmark layout models.")
     parser.add_argument(
         "--results_dir",
         type=str,
-        help="Path to JSON file with OCR results.",
+        help="Path to results directory",
         default=os.path.join(settings.RESULT_DIR, "benchmark"),
     )
-    parser.add_argument("--max", type=int, help="Maximum number of images to run benchmark on.", default=100)
+    parser.add_argument("--max", type=int, help="Maximum number of images to run benchmark on.", default=10)
     parser.add_argument("--debug", action="store_true", help="Run in debug mode.", default=False)
+    parser.add_argument(
+        "--model_type",
+        type=str,
+        choices=[e.value for e in ModelType],
+        default=ModelType.DOCLAYOUT_YOLO.value,
+        help="Type of model to use",
+    )
+    parser.add_argument(
+        "--dataset_type",
+        type=str,
+        choices=[e.value for e in DatasetType],
+        default=DatasetType.ELEM.value,
+        help="Type of dataset to use",
+    )
+    parser.add_argument("--dataset_path", type=str, help="Path to dataset", default=None)
     args = parser.parse_args()
 
-    pathname = "layout_bench"
-    # These have already been shuffled randomly, so sampling from the start is fine
-    dataset = datasets.load_dataset(settings.LAYOUT_BENCH_DATASET_NAME, split=f"train[:{args.max}]")
+    # Convert string arguments to enum types
+    model_type = ModelType(args.model_type)
+    dataset_type = DatasetType(args.dataset_type)
+
+    # Set dataset path if not provided
+    if dataset_type == DatasetType.ELEM:
+        dataset_path = settings.ELEM_LAYOUT_BENCH_DATASET_NAME
+    elif dataset_type == DatasetType.DOCLAYNET:
+        dataset_path = settings.DOCLAYNET_BENCH_DATASET_PATH
+    else:
+        raise ValueError(f"Unknown dataset type: {dataset_type}")
+
+    pathname = f"layout_bench_{model_type.value}_{dataset_type.value}"
+
+    # Load dataset
+    dataset = load_layout_dataset(dataset_path, dataset_type, max_samples=args.max)
     images = list(dataset["image"])
     images = convert_if_not_rgb(images)
 
-    det_model = load_det_model()
-    det_processor = load_det_processor()
-    layout_model = load_layout_yolo_model()
+    # Load model and processor
+    model, processor = get_model_and_processor(model_type)
 
     start = time.time()
-    line_predictions = batch_text_detection(images, det_model, det_processor)
-    layout_predictions = batch_layout_detection_yolo(images, layout_model, line_predictions)
+
+    if model_type == ModelType.DOCLAYOUT_YOLO:
+        layout_predictions = batch_layout_detection_yolo(images=images, model=model)
+    elif model_type == ModelType.ELEM_YOLO:
+        layout_predictions = batch_layout_detection_yolo(images=images, model=model)
+    else:
+        layout_predictions = batch_layout_detection(images=images, model=model, processor=processor)
+
     surya_time = time.time() - start
 
     folder_name = os.path.basename(pathname).split(".")[0]
     result_path = os.path.join(args.results_dir, folder_name)
     os.makedirs(result_path, exist_ok=True)
 
-    label_alignment = {  # First is publaynet, second is surya
-        "Image": [["Figure"], ["Picture", "Figure"]],
-        "Table": [["Table"], ["Table"]],
-        "Text": [["Text", "List"], ["Text", "Formula", "Footnote", "Caption", "List-item"]],
-        "Title": [["Title"], ["Section-header", "Title"]],
-    }
+    # Get label alignment based on dataset and model type
+    label_alignment = get_label_alignment(dataset_type, model_type)
 
     page_metrics = collections.OrderedDict()
     for idx, pred in enumerate(layout_predictions):
-        row = dataset[idx]
+        row = dataset.iloc[idx]
         all_correct_bboxes = []
+        all_pred_bboxes = []
         page_results = {}
         for label_name in label_alignment:
             correct_cats, surya_cats = label_alignment[label_name]
             correct_bboxes = [b for b, l in zip(row["bboxes"], row["labels"]) if l in correct_cats]
             all_correct_bboxes.extend(correct_bboxes)
             pred_bboxes = [b.bbox for b in pred.bboxes if b.label in surya_cats]
-
-            metrics = precision_recall(pred_bboxes, correct_bboxes, penalize_double=False)
+            all_pred_bboxes.extend(pred_bboxes)
+            # metrics = precision_recall_v2(pred_bboxes, correct_bboxes)
+            metrics = precision_recall_v2([], [])
             weight = len(correct_bboxes)
             metrics["weight"] = weight
             page_results[label_name] = metrics
@@ -76,8 +246,21 @@ def main():
         page_metrics[idx] = page_results
 
         if args.debug:
-            bbox_image = draw_bboxes_on_image(all_correct_bboxes, copy.deepcopy(images[idx]))
-            bbox_image.save(os.path.join(result_path, f"{idx}_layout.png"))
+            gt_image = visualize_bbox(
+                image_path=copy.deepcopy(images[idx]),
+                bboxes=row["bboxes"],
+                classes=row["labels"],
+                scores=[1] * len(row["bboxes"]),
+            )
+            pred_image = visualize_bbox(
+                image_path=copy.deepcopy(images[idx]),
+                bboxes=[i.bbox for i in layout_predictions[idx].bboxes],
+                classes=[i.label for i in layout_predictions[idx].bboxes],
+                scores=[i.confidence for i in layout_predictions[idx].bboxes],
+            )
+            # concat gt and pred images
+            concat_image = np.concatenate([gt_image, pred_image], axis=1)
+            Image.fromarray(concat_image).save(os.path.join(result_path, f"{idx}_layout.png"))
 
     mean_metrics = collections.defaultdict(dict)
     layout_types = sorted(page_metrics[0].keys())
@@ -101,19 +284,12 @@ def main():
     with open(os.path.join(result_path, "results.json"), "w+") as f:
         json.dump(out_data, f, indent=4)
 
-    table_headers = [
-        "Layout Type",
-    ] + metric_types
+    table_headers = ["Layout Type"] + metric_types
     table_data = []
     for layout_type in layout_types:
-        table_data.append(
-            [
-                layout_type,
-            ]
-            + [f"{mean_metrics[layout_type][m]:.5f}" for m in metric_types]
-        )
+        table_data.append([layout_type] + [f"{mean_metrics[layout_type][m]:.5f}" for m in metric_types])
 
-    print(tabulate(table_data, headers=table_headers, tablefmt="github"))
+    print(tabulate(table_data, headers=table_headers, tablefmt="markdown"))
     print(f"Took {surya_time / len(images):.5f} seconds per image, and {surya_time:.5f} seconds total.")
     print(
         "Precision and recall are over the mutual coverage of the detected boxes and the ground truth boxes at a .5 threshold."
