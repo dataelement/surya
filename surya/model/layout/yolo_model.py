cb007794230d11efd7105fa55305ad18f7840415
youjiachen <youmcgrady@126.com>
2024-11-18 12:05:19 +0800

    add print info for loading layout model

--- surya/model/layout/yolo_model.py
+++ surya/model/layout/yolo_model.py
@@ -6,4 +6,5 @@ from surya.settings import settings
 
 def load_model(ckpt=settings.LAYOUT_YOLO_CHECKPOINT) -> YOLO:
     layout_det_yolo = YOLO(ckpt)
+    print(f"Loaded layout model {ckpt}")
     return layout_det_yolo
